82b060ca16a67b27c51bcf85bed4a9f15acddb5b
mpb <165800@gmail.com>
2025-03-15 17:27:13 +0900

    Worked with Person

--- mypkg/mypkg/listener.py
+++ mypkg/mypkg/listener.py
@@ -1,12 +1,15 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16
+#from std_msgs.msg import Int16
+from person_msgs.msg import Person
 
 def cb(msg):
-	node.get_logger().info('listen %d'%msg.data)
+#	node.get_logger().info('listen %d'%msg.data)
+	node.get_logger().info('listen %s'%msg)
 
 rclpy.init()
 node = Node('listener')
-sub = node.create_subscription(Int16, 'countup',cb,10)
+#sub = node.create_subscription(Int16, 'countup',cb,10)
+sub = node.create_subscription(Person, 'person',cb,10)
 # くりかえす
 rclpy.spin(node)
--- mypkg/mypkg/talker.py
+++ mypkg/mypkg/talker.py
@@ -1,21 +1,26 @@
 import rclpy
 from rclpy.node import Node
-from  std_msgs.msg import Int16
+#from  std_msgs.msg import Int16
+from person_msgs.msg import Person
 
 
 rclpy.init()
 node = Node('talker')
 
-pub= node.create_publisher(Int16,'countup', 10)
+#pub= node.create_publisher(Int16,'countup', 10)
+pub= node.create_publisher(Person,'person', 10)
 n = 0
 
 def cb():
 	global n
 	# talkerがlistenerに送るmsgの
 	# 内容にあたる変数を型を指定して作成
-	msg = Int16()
+#	msg = Int16()
+	msg = Person()
 	# msgに内容を入れる
-	msg.data = n
+#	msg.data = n
+	msg.name = 'seo-san'
+	msg.age = n
 	# 送る
 	pub.publish(msg)
 	n += 1
